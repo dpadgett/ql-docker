4f6587f2d123b47812b1f077e451d23156eb13d6
Dan Padgett <dumbledore3@gmail.com>
2015-11-12 23:13:16 -0800

    add environment var to change the metro tag on the server name

--- server.sh
+++ server.sh
@@ -3,10 +3,15 @@ gameport=`expr $1 + 30960`
 rconport=`expr $1 + 31960`
 servernum=`expr $1 + 1`
 
+name="US Central"
+if [ "$SERVERNAME" != "" ]; then
+  name="$SERVERNAME"
+fi
+
 stdbuf -oL -eL /home/${USER}/Steam/steamapps/common/Quake\ Live\ Dedicated\ Server/run_server_x64_minqlx.sh \
     +set net_strict 1 \
     +set net_port $gameport \
-    +set sv_hostname "teh1337 Turbo CA [US Central]" \
+    +set sv_hostname "tBA* Turbo CA [$name]" \
     +set fs_homepath /home/${USER}/.quakelive/$gameport \
     +set zmq_rcon_enable 1 \
     +set zmq_rcon_password "quake1337!" \
